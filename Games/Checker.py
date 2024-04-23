56c8995eef0a2ae074d6bb253c58169807360ee8
Matthew-Somanlall <furious123.destroyer132@gmail.com>
2024-04-23 14:48:31 -0400

    Finished

--- Games/Checker.py
+++ Games/Checker.py
@@ -1,32 +1,33 @@
 def checker(n:int) -> str:
 
+    odd = '██  ' * (n // 2)
+    even = '  ██' * (n // 2)
     string = ''
 
     for i in range(n):
             
         if n / 2 == int(n / 2):
 
-            if i > 0:
-                string += '|\n'
             if i % 2 == 0:
-                string += f'{chr(9608)}{chr(9608)}  ' * ( n // 2 )
+                string += f'{odd}|\n'
+
             else:
-                string += f'  {chr(9608)}{chr(9608)}' * ( n // 2 )
+                string += f'{even}|\n'
 
         else:
             if i > 0:
                 string += '|\n'
             if i % 2 == 0:
-                string += f'{chr(9608)}{chr(9608)}  ' * ( n // 2 + 1 )
+                string += f'{odd}' + f'{chr(9608)}{chr(9608)}'
             else:
-                string += f'  {chr(9608)}{chr(9608)}' * ( n // 2  )
+                string += f'  {chr(9608)}{chr(9608)}' * ( n // 2 )
                 string += '  '
 
 
     return string
 
 
-print (checker(9))
+print (checker(1))
 
     
 
