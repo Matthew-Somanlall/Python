b179794c7521dc6b6766b47d32fa67701e4759df
Matthew-Somanlall <furious123.destroyer132@gmail.com>
2024-06-12 19:58:07 -0400

    Update task4_functions.py

--- School Work/Task 4/task4_functions.py
+++ School Work/Task 4/task4_functions.py
@@ -1,4 +1,5 @@
 from random import randrange
+from random import randint
 
 #-----------------------------------------------------------------------------
 def get_closest(values, target):
@@ -75,7 +76,7 @@ def rolling_averages(values, size):
 #-----------------------------------------------------------------------------
 def align_strings(strings, substr):
 
-
+    
 
     return ""
 
@@ -105,16 +106,44 @@ def deal_n_cards(cards, n):
 
 #-----------------------------------------------------------------------------
 def cut_the_cards(cards):
-    return None
+
+    """
+    """
+
+    cut_index = randint(1, len(cards)- 1)
+    bottom_part = cards[:cut_index]
+    top_part = cards[cut_index:]
+    cards.clear()
+    cards.extend(top_part)
+    cards.extend(bottom_part)
     
 
 #-----------------------------------------------------------------------------
 def shuffler(values):
-    return []
-
-
-
-
+    # Make a copy of the original list to avoid mutation
+    cards_copy = values.copy()
+    
+    # Cut the deck into two halves
+    cut_index = len(cards_copy) // 2
+    left_half = cards_copy[:cut_index]
+    right_half = cards_copy[cut_index:]
+    
+    # Initialize the new deck
+    new_deck = []
+    
+    # Merge the two halves by repeatedly taking the first card from the left half OR the last card from the right half
+    while left_half and right_half:
+        coin_flip = randint(1, 2)
+        if coin_flip == 1:
+            new_deck.append(right_half.pop())
+        elif coin_flip == 2:
+            new_deck.append(left_half.pop(0))
+    
+    # Add the remaining cards from the other half to the new deck
+    new_deck.extend(left_half)
+    new_deck.extend(right_half)
+ 
+    return new_deck
 #-----------------------------------------------------------------------------
 if __name__ == "__main__":
     '''
@@ -150,11 +179,11 @@ if __name__ == "__main__":
     #print(align_strings(words, "The"))
     
     
-    decks = make_decks(1)
-    hand = deal_n_cards(decks, 56)
-    print(len(hand), len(decks))
-    print(hand)
-    print(decks)
+    #decks = make_decks(1)
+    #hand = deal_n_cards(decks, 8)
+    #print(len(hand), len(decks))
+    #print(hand)
+    #print(decks)
     
     
     #cards = ['T♣', 'A♠', '7♥', '4♣', '3♦', 'K♦', 'K♥', '5♣']
@@ -164,12 +193,12 @@ if __name__ == "__main__":
     #print(cards)
     
     
-    #cards = ['T♣', 'A♠', '7♥', '4♣', '3♦', 'K♦', 'K♥', '5♣']
-    #cards = shuffler(cards)
-    #print(cards)
+    cards = ['T♣', 'A♠', '7♥', '4♣', '3♦', 'K♦', 'K♥', '5♣']
+    cards = shuffler(cards)
+    print(cards)
     
     
-    #cards = ['T♣', 'A♠', '7♥', '4♣', '3♦', 'K♦', 'K♥', '5♣']
-    #cards = shuffler(cards)
-    #print(cards)
+    cards = ['T♣', 'A♠', '7♥', '4♣', '3♦', 'K♦', 'K♥', '5♣']
+    cards = shuffler(cards)
+    print(cards)
     
